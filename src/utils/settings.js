c18fc1cf6e826061ce9d357e62095f653baa988f
박세준 <tpwns6693@gmail.com>
2025-12-14 00:33:43 +0900

    GM만 shinobi import를 할 수 있는 설정 추가

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -1,5 +1,15 @@
-function registerSettings() {
+import { CONSTANTS } from "../constants/constants.js";
 
+export function registerSettings() {
+    // @ts-ignore
+    game.settings?.register(CONSTANTS.MODULE_ID, "gmOnly", {
+        name: 'GM only',
+        hint: 'gm만 닌자를 불러올 수 있습니다.',
+        scope: 'world',     // "world" = sync to db, "client" = local storage
+        config: true,       // false if you dont want it to show in module config
+        type: Boolean,       // You want the primitive class, e.g. Number, not the name of the class as a string
+        default: false,
+        // requiresReload: true, // true if you want to prompt the user to reload
+    })
 }
 
-export { registerSettings };
